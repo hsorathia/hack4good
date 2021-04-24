f16a80dad420c73e18180a5c65ca8d3618ab2257
Habib Sorathia <has.sorathia@gmail.com>
2021-04-24 14:55:00 -0700

    setup api routes for ts

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// https://nextjs.org/docs/basic-features/typescript
 
-export default (req, res) => {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: 'John Doe' });
 };
+
+// import type { NextApiRequest, NextApiResponse } from 'next'
+
+// type Data = {
+//   name: string
+// }
+
+// export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+//   res.status(200).json({ name: 'John Doe' })
+// }
