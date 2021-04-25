1bc53abb08577110cc20347c9fa6ad52d0821fb2
Habib Sorathia <has.sorathia@gmail.com>
2021-04-24 17:43:33 -0700

    navbar wip

---
+++ Components/Layout/nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function NavBar(props: any) {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="/">home</a>
+          <a href="/test">test</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
