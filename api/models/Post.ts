781e2efe4aabbabe1d90aaef430a758ba814ae23
Jerry LEe <jerrylee1157@gmail.com>
2021-04-24 22:04:32 -0700

    Added login feature

---
+++ api/models/Post.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const PostSchema = new mongoose.Schema(
+  {
+    itemName: {
+      type: String,
+      required: true,
+    },
+    itemDescription: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    zipCode: {
+      type: String
+    },
+    condition: {
+      type: String
+    },
+    claimed: {
+      type: Boolean,
+      required: true
+    },
+    image: {
+      // Store Image URL
+      type: String
+    },
+
+  },
+  {
+    collection: 'Posts'
+  }
+);
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = {
+  PostSchema,
+  Post,
+};
